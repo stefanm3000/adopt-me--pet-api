277c039d608982ccfa1da79ebfb724abf0261c0e
StefanM3000 <stefanmilenkovicbusiness@gmail.com>
2020-11-26 16:56:03 +0100

    :construction_worker:

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from "react";
 import { render } from "react-dom";
 import SearchParams from "./SearchParams.js";
-import { Router } from "@reach/router";
+import { Link, Router } from "@reach/router";
 import Details from "./Details.js";
 
 const App = () => {
   return (
-    <div>
-      <h1 id="something-important">Adopt me!</h1>
-      <Router>
-        <SearchParams path="/" />
-        <Details path="/details/:id" />
-      </Router>
-    </div>
+    <React.StrictMode>
+      <div>
+        <header>
+          <Link to="/">Adopt me!</Link>
+        </header>
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
+      </div>
+    </React.StrictMode>
   );
 };
 
