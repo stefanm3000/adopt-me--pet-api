2f6c62c56f11389972eab539f75f816656163ddf
StefanM3000 <stefanmilenkovicbusiness@gmail.com>
2020-11-26 16:02:56 +0100

    :construction:

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { render } from "react-dom";
-import SearchParams from './SearchParams.js';
+import SearchParams from "./SearchParams.js";
+import { Router } from "@reach/router";
+import Details from "./Details.js";
 
 const App = () => {
   return (
     <div>
       <h1 id="something-important">Adopt me!</h1>
-      <SearchParams />
+      <Router>
+        <SearchParams path="/" />
+        <Details path="/details/:id" />
+      </Router>
     </div>
   );
 };
--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,28 +1,42 @@
 import React, { useState, useEffect } from "react";
 import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown";
+import Results from './Results';
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
   const [breeds, setBreeds] = useState([]);
   const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+  const [pets, setPets] = useState([]);
+
+  async function requestPets() {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal,
+    });
+
+    setPets(animals || []);
+  }
 
   useEffect(() => {
     setBreeds([]);
     setBreed("");
 
-    pet.breeds(animal).then(({ breeds }) => {
-      const breedStrings = breeds.map(({ name }) => name);
+    pet.breeds(animal).then(({ breeds: apiBreeds }) => {
+      const breedStrings = apiBreeds.map(({ name }) => name);
       setBreeds(breedStrings);
     }, console.error);
   }, [animal, setBreed, setBreeds]);
 
-  console.log("state of location", location);
 
   return (
     <div className="search-params">
-      <form>
+      <form onSubmit={() => {
+          e.preventDefault();
+          requestPets();
+      }}>
         <label htmlFor="location">
           <input
             id="location"
@@ -35,6 +49,7 @@ const SearchParams = () => {
         <BreedDropdown />
         <button>Submit</button>
       </form>
+      <Results pets={pets} />
     </div>
   );
 };
