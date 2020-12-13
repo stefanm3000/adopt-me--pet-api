0180e7ab38da678579387a9f8a874cec9688ea9e
StefanM3000 <stefanmilenkovicbusiness@gmail.com>
2020-12-13 19:27:47 +0100

    :construction:

--- src/App.js
+++ src/App.js
@@ -1,27 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import { render } from "react-dom";
-import SearchParams from "./SearchParams.js";
-import { Link, Router } from "@reach/router";
-import Details from "./Details.js";
-import ThemeContext from "./ThemeContext";
 
 const App = () => {
-  const themeHook = useState("lightgreen");
-  return (
-    <React.StrictMode>
-      <ThemeContext.Provider value={themeHook}>
-        <div>
-          <header>
-            <Link to="/">Adopt me!</Link>
-          </header>
-          <Router>
-            <SearchParams path="/" />
-            <Details path="/details/:id" />
-          </Router>
-        </div>
-      </ThemeContext.Provider>
-    </React.StrictMode>
-  );
+  return <div>one more time</div>;
 };
 
 render(<App />, document.getElementById("root"));
