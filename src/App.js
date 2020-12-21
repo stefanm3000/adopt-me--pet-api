94e5395ab304f73f5a25c85a2787c759cf146379
StefanM3000 <stefanmilenkovicbusiness@gmail.com>
2020-12-21 14:23:19 +0100

    :construction:

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
-import { render } from "react-dom";
+import ReactDOM from "react-dom";
+import Pet from "./Pet";
 
 const App = () => {
-  return <div>one more time</div>;
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="dog" breed="Havanese" />
+      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="cat" breed="Mix" />
+    </div>
+  );
 };
 
-render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
