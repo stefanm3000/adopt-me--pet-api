5d5e79726e6b2e564ca9edbcf6ad35fc8ac7a07e
StefanM3000 <stefanmilenkovicbusiness@gmail.com>
2020-12-23 17:30:42 +0100

    :poop:

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SearchParams from "./SearchParams";
-import { Router } from "@reach/router";
+import { Link, Router } from "@reach/router";
 import Details from "./Details";
 
 const App = () => {
   return (
     <React.StrictMode>
       <div>
-        <h1 id="something-important">Adopt Me!</h1>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
         <Router>
           <SearchParams path="/" />
           <Details path="/details/:id" />
